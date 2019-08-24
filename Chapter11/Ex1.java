6e7def0fef091a4ff0deb743762194452636e4a2
Isabella DeBoer <isabella@songdeboer.net>
2019-08-24 11:52:32 -0600

    add second variable

--- Chapter11/Ex1.java
+++ Chapter11/Ex1.java
@@ -2,8 +2,10 @@ public class Ex1
 {
     public static void main( String[] args )
     {
-        String str = new String("Hello World")
+        String str = new String("Hello World");
+        String secondVariable = new String("second variable");
 
-        System.out.println( str )
+        System.out.println( str );
+        System.out.println( secondVariable );
     }
 }
