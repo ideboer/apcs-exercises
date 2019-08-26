c5e84d8adb39bf7241655ffd12c1e9bfa5b5a473
Isabella DeBoer <isabella@songdeboer.net>
2019-08-26 12:13:51 -0600

    expirement with spaces and tabs

--- Chapter11/Ex3.java
+++ Chapter11/Ex3.java
@@ -2,6 +2,12 @@ public class Ex3
 {
     public static void main( String[] args )
     {
-        
+        String first = new String(" I am testing out this new    command  ");
+        String second;
+
+        second = first.trim();
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
