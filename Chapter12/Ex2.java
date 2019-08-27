f1ddd5c5f6b7d849c35352c12f586f60e8a47386
Isabella DeBoer <isabella@songdeboer.net>
2019-08-27 12:17:08 -0600

    convert cents to dollars

--- Chapter12/Ex2.java
+++ Chapter12/Ex2.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Ex2
 {
-    public static void main( String{} args )
+    public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int dollars, cents, input;
+
+        System.out.println( "Input the cents: " );
+        input = scan.nextInt();
+
+        dollars = input / 100;
+        cents = input % 100;
+
+        System.out.println( "That is " + dollars + " dollars and " + cents + " cents.");
+
     }
 }
