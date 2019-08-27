a357d405bb0f4e0232f0138bee43dad7d66c6125
Isabella DeBoer <isabella@songdeboer.net>
2019-08-27 12:36:27 -0600

    state the correct change

--- Chapter12/Ex3.java
+++ Chapter12/Ex3.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Ex3
 {
-    public static void main( String{} args )
+    public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int dollars, quarters, dimes, nickels, cents, dollars_remainder, quarters_remainder, dimes_remainder, input;
+
+        System.out.println("Input your cents: ");
+        input = scan.nextInt();
+
+        dollars = input / 100;
+        dollars_remainder = input % 100;
+        quarters = dollars_remainder / 25;
+        quarters_remainder = dollars_remainder % 25;
+        dimes = quarters_remainder / 10;
+        dimes_remainder = quarters_remainder % 10;
+        nickels = dimes_remainder / 5;
+        cents = dimes_remainder % 5;
+
+        System.out.println( "Your change is: " + dollars + " dollars, " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, and " + cents + " cents.");
+
     }
 }
