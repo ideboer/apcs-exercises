1d65ad25607e105c722fedfd1b5653e49d3a722e
Isabella DeBoer <isabella@songdeboer.net>
2019-08-27 12:47:28 -0600

    find the current

--- Chapter12/Ex4.java
+++ Chapter12/Ex4.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Ex4
 {
-    public static void main( String{} args )
+    public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int voltage, resistance;
+        float current;
+
+        System.out.println("Input the voltage: ");
+        voltage = scan.nextInt();
+        System.out.println("Input the resistance: ");
+        resistance = scan.nextInt();
+
+        current = (float)voltage / resistance;
+
+        System.out.println( "The current is : " + current);
+
     }
 }
