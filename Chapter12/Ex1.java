98ebbe492d7e90cd59833de8879f0cfe95a0689d
Isabella DeBoer <isabella@songdeboer.net>
2019-08-27 12:09:11 -0600

    find the area

--- Chapter12/Ex1.java
+++ Chapter12/Ex1.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Ex1
 {
-    public static void main( String{} args )
+    public static void main( String[] args )
     {
-        
+       Scanner scan = new Scanner( System.in );
+       int radius;
+
+       System.out.println( "Input the radius: " );
+       radius = scan.nextInt();
+
+       System.out.println( "The radius is: " + radius );
+       System.out.println( "The area is: " + (Math.PI * radius * radius) );
     }
 }
