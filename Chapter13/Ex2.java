5bd457b3e4034bd570a19836ad82fce8b9aa8876
Isabella DeBoer <isabella@songdeboer.net>
2019-08-28 12:10:41 -0600

    find distance

--- Chapter13/Ex2.java
+++ Chapter13/Ex2.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 
-public class Ex1
+public class Ex2
 {
-    public static void main( String[] args );
+    public static void main( String[] args )
     {
+        Scanner scan = new Scanner( System.in );
+        Double seconds, distance;
+        Double gravity = 32.174;
+
+        System.out.println("Enter the number of sceonds: ");
+        seconds = scan.nextDouble();
         
+        distance = (gravity * seconds * seconds) / 2;
+
+        System.out.println("Distance: " + distance);
     }
 }
--- Chapter13/Ex4.java
+++ Chapter13/Ex4.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ex1
 {
-    public static void main( String[] args );
+    public static void main( String[] args )
     {
         
     }
