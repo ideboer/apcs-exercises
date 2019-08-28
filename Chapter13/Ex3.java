fe7342cdb7d6af24035686c816a2a872a5ac9688
Isabella DeBoer <isabella@songdeboer.net>
2019-08-28 12:18:02 -0600

    find the base 2 log

--- Chapter13/Ex3.java
+++ Chapter13/Ex3.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
-public class Ex1
+public class Ex3
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner(System.in);
+        Double X, answer;
+
+        System.out.println("Enter a double: ");
+        X = scan.nextDouble();
+
+        answer = Math.log(X) / Math.log(2);
+
+        System.out.println("Base 2 log of " + X + " is " + answer);
     }
 }
