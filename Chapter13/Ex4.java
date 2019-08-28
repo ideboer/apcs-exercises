6a88f5579518fe7f54bdf29e0bd65cc7c3db272f
Isabella DeBoer <isabella@songdeboer.net>
2019-08-28 12:22:58 -0600

    find harmonic mean

--- Chapter13/Ex4.java
+++ Chapter13/Ex4.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
-public class Ex1
+public class Ex4
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        Double X, Y, arithmetic_mean, harmonic_mean;
+
+        System.out.println("Enter X: ");
+        X = scan.nextDouble();
+        System.out.println("Enter Y: ");
+        Y = scan.nextDouble();
+
+        arithmetic_mean = (X + Y) / 2;
+        harmonic_mean = 2 / ((1 / X) + (1 / Y));
+
+        System.out.println("Arithemtic mean: " + arithmetic_mean);
+        System.out.println("Harmonic mean: " + harmonic_mean);
+
     }
 }
