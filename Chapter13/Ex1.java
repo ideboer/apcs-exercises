e7a9181858b73106a9648cf9bc86c80125b0c541
Isabella DeBoer <isabella@songdeboer.net>
2019-08-28 12:05:37 -0600

    find annual cost

--- Chapter13/Ex1.java
+++ Chapter13/Ex1.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Ex1
 {
-    public static void main( String[] args );
+    public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in);
+        Double cost, hours, annual_cost;
+
+        System.out.println("Enter cost per kilowatt-hour: ");
+        cost = scan.nextDouble();
+        System.out.println("Enter kilowatt-hours used per year: ");
+        hours = scan.nextDouble();
+
+        annual_cost = cost * hours / 100;
+
+        System.out.println("Annual cost: " + annual_cost);
     }
 }
