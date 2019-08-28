cb0c2fc752e101aa7e5cbc970f45ba87779115a3
Isabella DeBoer <isabella@songdeboer.net>
2019-08-28 11:55:49 -0600

    create substring

--- Chapter12/Ex5.java
+++ Chapter12/Ex5.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Ex5
 {
-    public static void main( String{} args )
+    public static void main( String[] args )
     {
-        Scanner can = new Scanner( System.in );
+        Scanner scan = new Scanner( System.in );
         int begin_index, end_index;
+        String string;
+
+        System.out.println("Input a string: ");
+        string = scan.nextLine();
+        System.out.println("Input the beginning index: ");
+        begin_index = scan.nextInt();
+        System.out.println("Input the end index: ");
+        end_index = scan.nextInt();
+
+        String newString = string.substring(begin_index, end_index);
+
+        System.out.println("Original string: " + string);
+        System.out.println("substring: " + newString);
+
         
     }
 }
