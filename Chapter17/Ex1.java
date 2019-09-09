15e79bf046d1b92ce93f830882f1c2c51a412e5c
Isabella DeBoer <isabella@songdeboer.net>
2019-09-09 12:48:35 -0600

    add printed lines

--- Chapter17/Ex1.java
+++ Chapter17/Ex1.java
@@ -4,6 +4,19 @@ public class Ex1
 {
     public static void main( String[] args )
     {
+        String item;
+        int delivery;
+        double price;
+
+        System.out.println("Enter the item: ");
+        item = scan.nextLine();
+
+        System.out.println("Enter the price: ");
+        price = scan.nextInt();
+
+        System.out.println("Express delivery? (0==no, 1==yes): ");
+        delivery = scan.nextInt();
+
         
     }
 }
