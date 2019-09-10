acf6725d7c327741a84b35dac658064e52c9ac44
Isabella DeBoer <isabella@songdeboer.net>
2019-09-10 12:24:53 -0600

    print out invoice

--- Chapter17/Ex1.java
+++ Chapter17/Ex1.java
@@ -4,19 +4,38 @@ public class Ex1
 {
     public static void main( String[] args )
     {
+        Scanner scan = new Scanner( System.in );
         String item;
         int delivery;
-        double price;
+        double item_price, total_price;
+        double deliv_price = 0;
 
         System.out.println("Enter the item: ");
         item = scan.nextLine();
 
         System.out.println("Enter the price: ");
-        price = scan.nextInt();
+        item_price = scan.nextDouble();
 
         System.out.println("Express delivery? (0==no, 1==yes): ");
         delivery = scan.nextInt();
 
+        if (item_price < 10.0)
+        {
+            deliv_price +=2;
+        }
+
+        if (delivery == 1)
+        {
+            deliv_price += 3;
+        }
+
+        total_price = deliv_price + item_price;
+
+        System.out.println("Invoice: ");
+        System.out.println("\t" + item + "\t" + item_price);
+        System.out.println("\tDelivery" + "\t" + deliv_price);
+        System.out.println("\ttotal" + "\t" + total_price);
+        
         
     }
 }
