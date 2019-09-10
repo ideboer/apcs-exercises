0dac2a24aee6ed43d945e6c8f6fca4fced656dda
Isabella DeBoer <isabella@songdeboer.net>
2019-09-10 12:49:19 -0600

    find the price

--- Chapter17/Ex5.java
+++ Chapter17/Ex5.java
@@ -4,6 +4,26 @@ public class Ex5
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int pounds, price, amount_over = 0;
+
+        price = 20;
+
+        System.out.println("Enter weight of the load of trash: ");
+        pounds = scan.nextInt();
+
+        if (pounds > 200)
+        {
+            amount_over = (pounds - 200) / 100;
+
+            if ( ( (pounds - 200) % 100 ) > 0 )
+            {
+                amount_over += 1;
+            }
+        }
+
+        price += (amount_over * 8) ;
+
+        System.out.println("Charge: " + price);
     }
 }
