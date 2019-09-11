140466de6dafc2852fb719c50fa0e7ca89bdc0d7
Isabella DeBoer <isabella@songdeboer.net>
2019-09-11 13:03:08 -0600

    add count to end feature

--- Chapter19/Ex1.java
+++ Chapter19/Ex1.java
@@ -4,6 +4,19 @@ public class Ex1
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int count, end;
+
+        System.out.println("Enter Start: ");
+        count = scan.nextInt();
+
+        System.out.println("Enter End: ");
+        end = scan.nextInt();
+
+        while (count <= end)
+        {
+            System.out.println(count);
+            count += 1;
+        }
     }
 }
