6a6579f82a88069efc2f69932f899ba79f02f61f
Isabella DeBoer <isabella@songdeboer.net>
2019-09-11 13:08:23 -0600

    initialize numbers

--- Chapter19/Ex2.java
+++ Chapter19/Ex2.java
@@ -4,6 +4,11 @@ public class Ex2
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int n, sum, count;
+
+        System.out.println("Enter N: ");
+
+
     }
 }
