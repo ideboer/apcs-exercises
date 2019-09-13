f237b2a0dd185291e42494ff94d835e0f021589e
Isabella DeBoer <isabella@songdeboer.net>
2019-09-13 12:43:29 -0600

    add together integers

--- Chapter20/Ex1.java
+++ Chapter20/Ex1.java
@@ -1,9 +1,24 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Ex1
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        int num_of_int, count = 0;
+        int sum = 0;
+
+        System.out.println("How many integers will be added: ");
+        num_of_int = scan.nextInt();
+
+        while (count < num_of_int)
+        {
+            System.out.println("Enter an integer: ");
+            sum += scan.nextInt();
+
+            count ++;
+        }
+
+        System.out.println("the sum is " + sum);
     }
 }
