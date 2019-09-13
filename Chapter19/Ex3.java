29440df4a52422a45c96890b391a189df129c721
Isabella DeBoer <isabella@songdeboer.net>
2019-09-13 12:25:09 -0600

    add dots until the total legth is 30

--- Chapter19/Ex3.java
+++ Chapter19/Ex3.java
@@ -4,6 +4,26 @@ public class Ex3
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        String first, second;
+        int dots_num, i = 0;
+
+        System.out.println("Enter first word: ");
+        first = scan.nextLine();
+
+        System.out.println("Enter second word: ");
+        second = scan.nextLine();
+
+        dots_num = 30 - (first.length() + second.length());
+
+        System.out.print(first);
+
+        while ( dots_num > i )
+        {
+            System.out.print(".");
+            i += 1;
+        }
+
+        System.out.print(second);
     }
 }
