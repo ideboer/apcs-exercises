0bd36d97230078b88aca2888ad94f84475abb81e
Isabella DeBoer <isabella@songdeboer.net>
2019-09-13 12:37:38 -0600

    Initial commit

---
+++ Chapter20/Ex1.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+
+public class Ex1
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ Chapter20/Ex2.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+
+public class Ex2
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
