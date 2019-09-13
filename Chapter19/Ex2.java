fb633b233d0683ff8fa34db679fbe68bcf3443bf
Isabella DeBoer <isabella@songdeboer.net>
2019-09-13 12:09:46 -0600

    find sum through a loop and a formula

--- Chapter19/Ex2.java
+++ Chapter19/Ex2.java
@@ -5,10 +5,24 @@ public class Ex2
     public static void main( String[] args )
     {
         Scanner scan = new Scanner( System.in );
-        int n, sum, count;
+        int n, loop_sum = 0;
+        int formula_sum = 0;
+        int count = 0;
 
         System.out.println("Enter N: ");
+        n = scan.nextInt();
 
+        while (count <= n)
+        {
+            loop_sum += count;
+            count += 1;
+        }
+
+        System.out.println("Loop Sum: " + loop_sum);
+
+        formula_sum = ( n * ( n + 1 ) ) / 2;
+
+        System.out.println("Formula Sum: " + formula_sum);
 
     }
 }
