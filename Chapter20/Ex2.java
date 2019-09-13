55357232883acaabbd31d7f37771cf3ae52f2f5c
Isabella DeBoer <isabella@songdeboer.net>
2019-09-13 12:51:38 -0600

    find harmonic sum

--- Chapter20/Ex2.java
+++ Chapter20/Ex2.java
@@ -1,9 +1,23 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Ex2
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        double sum = 0;
+        double count = 1;
+        double n;
+
+        System.out.println("Enter N");
+        n = scan.nextDouble();
+
+        while( count <= n)
+        {
+            sum += (1.0 / count);
+            count ++;
+        }
+
+        System.out.println("Sum is: " + sum);
     }
 }
