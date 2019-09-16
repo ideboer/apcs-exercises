b102a21a2dfc0e817fbc30357aa629f787019673
Isabella DeBoer <isabella@songdeboer.net>
2019-09-16 12:48:40 -0600

    find power of a number with a loop

--- Chapter21/Ex3V2.java
+++ Chapter21/Ex3V2.java
@@ -4,6 +4,30 @@ public class Ex3V2
 {
     public static void main( String [] args )
     {
+        Scanner scan = new Scanner( System.in );
+        double X, total = 1;
+        int N, count = 1;
+
+        System.out.println("Enter X");
+        X = scan.nextDouble();
+        System.out.println("Enter N");
+        N = scan.nextInt();
+
+        if (N < 0)
+        {
+            System.out.println("N must be a positive integer");
+        }
+        else
+        {
+            while (count <= N)
+            {
+                total = total * X;
+                count ++;
+            }
+
+            System.out.println(X + " raised to the power of " + N + " is: " + total);
+        }
+
         
     }
 }
