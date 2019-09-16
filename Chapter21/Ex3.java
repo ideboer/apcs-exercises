784705c86f02eeb33999a5e56b235ae5dc0dd116
Isabella DeBoer <isabella@songdeboer.net>
2019-09-16 12:43:22 -0600

    find power of a number with Math.pow

--- Chapter21/Ex3.java
+++ Chapter21/Ex3.java
@@ -4,6 +4,26 @@ public class Ex3
 {
     public static void main( String [] args )
     {
+        Scanner scan = new Scanner( System.in );
+        double X, total;
+        int N;
+
+        System.out.println("Enter X");
+        X = scan.nextDouble();
+        System.out.println("Enter N");
+        N = scan.nextInt();
+
+        if (N < 0)
+        {
+            System.out.println("N must be a positive integer");
+        }
+        else
+        {
+            total = Math.pow(X, N);
+
+            System.out.println(X + " raised to the power of " + N + " is: " + total);
+        }
+
         
     }
 }
