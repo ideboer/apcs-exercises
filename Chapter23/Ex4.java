bafecb425fe4d4715700b9cc73f6a2c2e0d169c2
Isabella DeBoer <isabella@songdeboer.net>
2019-09-17 12:52:59 -0600

    Basic code

--- Chapter23/Ex4.java
+++ Chapter23/Ex4.java
@@ -4,6 +4,13 @@ public class Ex4
 {
     public static void main( String[] args )
     {
+        Scanner scan = new Scanner( System.in );
+        int x;
+        double e = Math.exp(1);
+
+        System.out.println("enter X: ");
+        x = scan.nextInt();
+
         
     }
 }
