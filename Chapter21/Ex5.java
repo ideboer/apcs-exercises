2dfb659c359ee6cf52bbdceedbc6d19942e688cc
Isabella DeBoer <isabella@songdeboer.net>
2019-09-17 12:17:41 -0600

    create and grade a multiplication test

--- Chapter21/Ex5.java
+++ Chapter21/Ex5.java
@@ -5,6 +5,35 @@ public class Ex5
 {
     public static void main( String [] args )
     {
+        Scanner scan = new Scanner( System.in );
+        Random random = new Random();
+        int random1, random2, answer, count = 1;
+        int numCorrect = 0;
         
+        while (count < 10)
+        {
+            random1 = random.nextInt(10);
+            random2 = random.nextInt(10);
+
+            System.out.println("What is " + random1 + " * " + random2 + "?");
+            answer = scan.nextInt();
+
+            if (answer == (random1 * random2))
+            {
+                System.out.println("Right!");
+                numCorrect ++;
+            }
+            else
+            {
+                System.out.println("wrong. " + random1 + " * " + random2 + " is " + (random1 * random2));
+            }
+
+            System.out.println(" ");
+
+            count ++;
+
+        }
+        System.out.println("You got " + numCorrect + " out of 10 correct.");
+
     }
 }
