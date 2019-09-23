8e0e90ce319b9e69bc2731544789b820488791dc
Isabella DeBoer <isabella@songdeboer.net>
2019-09-23 12:46:04 -0600

    find the number of sheep in a herd

--- Chapter24/Ex1.java
+++ Chapter24/Ex1.java
@@ -4,6 +4,22 @@ public class Ex1
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        double N, years = 0;
+        double power = 1;
+
+        for ( int t = 0; t < 25; t++)
+        {
+            power *= 0.83;
+            N = 220 / (1 + 10 * power );
+            System.out.println("N: " + N);
+
+            if (years == 0 && N >= 80)
+            {
+                years = t;
+            }
+        }
+
+        System.out.println("the sheep must be supervised for " + years + " years.");
     }
 }
