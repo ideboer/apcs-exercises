e65ec0187ea3508ccfb1d0ea9756102414b3822b
Isabella DeBoer <isabella@songdeboer.net>
2019-09-23 12:21:45 -0600

    find the sum of all odd integers from 1 to n and find n^2

--- Chapter24/Ex2.java
+++ Chapter24/Ex2.java
@@ -4,6 +4,20 @@ public class Ex2
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        System.out.println("Enter a number:");
+        int n = scan.nextInt();
+        int sum = 0;
+
+        for ( int count = 1; count <= n; count ++)
+        {
+            if ( (count % 2) == 1 )
+            {
+                sum += count;
+            }
+        }
+
+        System.out.println("sum of all odd integers: " + sum);
+        System.out.println("n^2: " + (n * n) );
     }
 }
