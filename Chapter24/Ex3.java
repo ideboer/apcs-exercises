981be364101e715d8c181684402d9b1ea0da3374
Isabella DeBoer <isabella@songdeboer.net>
2019-09-23 12:53:06 -0600

    does not work

--- Chapter24/Ex3.java
+++ Chapter24/Ex3.java
@@ -4,6 +4,28 @@ public class Ex3
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        System.out.println("Enter N: ");
+        int N = scan.nextInt();
+        int sum = 0;
+
+        for ( int count = 1; count <= N; count++ )
+        {
+            if ( (N % count) == 0 )
+            {
+                sum += count;
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+
+        if (sum == N)
+        {
+            System.out.println("N is a perfect number");
+        }
+        else
+        {
+            System.out.println("not a perfect number")
+        }
     }
 }
