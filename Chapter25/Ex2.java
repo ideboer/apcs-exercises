26d7954ff54f99d855b52df4e37fd8ba82c8fc1f
Isabella DeBoer <isabella@songdeboer.net>
2019-09-24 12:46:54 -0600

    find guests needed and probability

--- Chapter25/Ex2.java
+++ Chapter25/Ex2.java
@@ -4,6 +4,27 @@ public class Ex2
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        System.out.println("enter number of guests: ");
+        int n = scan.nextInt();
+        double probability = 1;
+
+        for (double count = 1; count < n; count ++)
+        {
+            probability *= ( (365 - count) / 365 );
+        }
+
+        System.out.println("probability that no guests share a birthday: " + probability);
+
+        double probability2 = 1;
+        int people = 0;
+
+        for (double days = 365; probability2 > 0.50; days--)
+        {
+            probability2 *= (days / 365);
+            people ++;
+        }
+
+        System.out.println(people + " guests are needed so there is a 50% chance that 2 share the same birthday.");
     }
 }
