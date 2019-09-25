e17405989ac3c9ead9f8d40c497e3a8d562ae96e
Isabella DeBoer <isabella@songdeboer.net>
2019-09-25 07:34:02 -0600

    find perfect square and triangle numbers

--- Chapter25/Ex3.java
+++ Chapter25/Ex3.java
@@ -4,6 +4,40 @@ public class Ex3
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        System.out.println("Enter an upper limit: ");
+        double upperLimit = scan.nextDouble();
+        boolean perfectSquare = false;
+        boolean triangle = false;
+
+        for (int number = 1; number <= upperLimit; number++)
+        {
+            for (int i = 1; i * i <= number; i++)
+            {
+                if ( (number % i) == 0 && (number / i ) == i)
+                {
+                    perfectSquare = true;
+                }
+            }
+
+            int originalNumber = number;
+
+            for (int count = 1; count <= originalNumber; count++)
+            {
+                originalNumber -= count;
+                if ( originalNumber == 0 )
+                {
+                    triangle = true;
+                }
+            }
+
+            if (perfectSquare && triangle)
+            {
+                System.out.println(number);
+            }
+
+            perfectSquare = false;
+            triangle = false;
+        }
     }
 }
