2cb804362ed4238ede9edb7f3869564ca82851ad
Isabella DeBoer <isabella@songdeboer.net>
2019-10-01 12:42:54 -0600

    ant walk

--- Chapter30/Ex7.java
+++ Chapter30/Ex7.java
@@ -4,6 +4,22 @@ public class Ex7
 {
     public static void main( String[] args )
     {
-        
+        Random random = new Random();
+
+        double X = 0.0;
+        double Y = 0.0;
+
+        int iterations = 100;
+
+        for (int count = 0; count < iterations; count ++)
+        {
+            X += ( 2 * random.nextDouble() - 1);
+            Y += ( 2 * random.nextDouble() - 1);
+        }
+
+        double distance = Math.sqrt( (X * X) + (Y * Y) );
+
+        System.out.println("Coordinates are: (" + X + ", " + Y + "). ");
+        System.out.println("Distance from the origin is: " + distance);
     }
 }
