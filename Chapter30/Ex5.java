3746ca1f28af03620f321f1b51e6528834589258
Isabella DeBoer <isabella@songdeboer.net>
2019-10-01 12:33:42 -0600

    created a password cracker

--- Chapter30/Ex5.java
+++ Chapter30/Ex5.java
@@ -4,6 +4,33 @@ public class Ex5
 {
     public static void main( String[] args )
     {
+        Scanner scan = new Scanner( System.in );
+        Random random = new Random();
         
+        String choices = "abcdefghijklmnopqrstuvwxyz";
+        boolean guess = false;
+        String password = "";
+
+        System.out.println("Enter a secret password: ");
+        String realPassword = scan.nextLine();
+
+        System.out.println("Here is your password: " + realPassword);
+        int i;
+
+        for (i = 0; !guess; i++)
+        {
+            for (int count = 0; count < 5; count ++)
+            {
+                password = password + choices.charAt( random.nextInt(choices.length() ) );
+                if (password.equals(realPassword))
+                {
+                    guess = true;
+                }
+            }
+            password = "";
+        }
+
+        System.out.println("It took " + i + " tries to guess it.");
+
     }
 }
