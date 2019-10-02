d0fcb7357c61a01cef5a28ea11ea00c4e0c29623
Isabella DeBoer <isabella@songdeboer.net>
2019-10-02 07:40:55 -0600

    finished game

--- Chapter30/Ex8.java
+++ Chapter30/Ex8.java
@@ -4,12 +4,17 @@ public class Ex8
 {
     public static void main( String[] args )
     {
-        int rounds;
+        Random random = new Random();
+        Scanner scan = new Scanner( System.in );
+        
+        int rounds, firstWins = 0;
+        int secondWins = 0;
         String firstPlayer, secondPlayer;
         double firstGuess, secondGuess, firstAway, secondAway;
         
         System.out.println("How many rounds? ");
         rounds = scan.nextInt();
+        scan.nextLine();
 
         System.out.println("First player, sign in");
         firstPlayer = scan.nextLine();
@@ -17,36 +22,56 @@ public class Ex8
         System.out.println("Second player, sign in");
         secondPlayer = scan.nextLine();
 
-        for (count = 0; count < rounds; count++)
+        for (int count = 0; count < rounds; count++)
         {
-            double number = 100 * random.nextDouble();
+            double number = 99 * random.nextDouble() + 1;
 
             System.out.println("What is the square root of " + number + "?");
 
             if (count % 2 == 1)
             {
-                System.out.println(firstPlayer + ", your guess: ")
+                System.out.println(firstPlayer + ", your guess: ");
                 firstGuess = scan.nextDouble();
 
-                System.out.println(secondPlayer + ", your guess: ")
+                System.out.println(secondPlayer + ", your guess: ");
                 secondGuess = scan.nextDouble();
             }
             else
             {
-                System.out.println(secondPlayer + ", your guess: ")
+                System.out.println(secondPlayer + ", your guess: ");
                 secondGuess = scan.nextDouble();
 
-                System.out.println(firstPlayer + ", your guess: ")
+                System.out.println(firstPlayer + ", your guess: ");
                 firstGuess = scan.nextDouble();
             }
 
-            correctNum = Math.sqrt(number);
+            double correctNum = Math.sqrt(number);
 
-            firstAway = Math.abs(correct - firstGuess);
-            secondAway = Math.abs(correct - secondGuess);
+            firstAway = Math.abs(correctNum - firstGuess);
+            secondAway = Math.abs(correctNum - secondGuess);
 
-            if
+            System.out.println("The correct square root: " + correctNum);
+            System.out.println(firstPlayer + " is " + firstAway + " away");
+            System.out.println(secondPlayer + " is " + secondAway + " away");
+
+            if (firstAway < secondAway)
+            {
+                firstWins += 1;
+                System.out.println(firstPlayer + " wins!");
+            }
+            else if (secondAway < firstAway)
+            {
+                secondWins += 1;
+                System.out.println(secondPlayer + " wins!");
+            }
+            else
+            {
+                System.out.println("Its a tie!");
+            }
         }
+
+        System.out.println("---- Final Score ----");
+        System.out.println(firstPlayer + ":\t" + firstWins + "\t" + secondPlayer + ": \t" + secondWins);
         
     }
 }
--- Chapter30/Ex9.java
+++ Chapter30/Ex9.java
@@ -4,6 +4,55 @@ public class Ex9
 {
     public static void main( String[] args )
     {
-        
+        Scanner scan = new Scanner( System.in );
+        Random random = new Random();
+
+        int playerPoints = 0;
+        int compPoints = 0;
+        int workingScore = 0;
+        String answer = "y";
+        int roll;
+
+        while (playerPoints < 100 && compPoints < 100)
+        {
+            compPoints = 0;
+            compPoints += (random.nextInt(6) + 1) + (random.nextInt(6) + 1) + (random.nextInt(6) + 1);
+
+            System.out.println("roll? (y or n)");
+            answer = scan.nextLine();
+            
+            while (answer.equals("y"))
+            {
+                roll = 0;
+                roll = (random.nextInt(6) + 1);
+                System.out.println("you rolled: " + roll);
+                if (roll == 1)
+                {
+                    workingScore = 0;
+                    answer = "n";
+                }
+                else
+                {
+                    workingScore += roll;
+                    System.out.println("roll? (y or n)");
+                    answer = scan.nextLine();
+                }
+
+            }
+
+            playerPoints += workingScore;
+            workingScore = 0;
+
+            System.out.println("player points: " + playerPoints + " comp points" + compPoints);
+        }
+
+        if (playerPoints >= 100)
+        {
+            System.out.println("player wins!");
+        }
+        else
+        {
+            System.out.println("computer wins.");
+        }
     }
 }
