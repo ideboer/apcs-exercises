0d389f0f656a1977d2769411c069ee63aa029b36
Isabella DeBoer <isabella@songdeboer.net>
2019-11-19 12:46:11 -0700

    finished odious numbers

--- Chapter54/odiousNumbers.java
+++ Chapter54/odiousNumbers.java
@@ -1,30 +1,54 @@
 import  java.util.Scanner;
 
-public class odiousNumber
+public class odiousNumbers
 {
   
   // Calculate num factorial
-  public static long binary( int N )
-  {
-     return 1L;
+  public static boolean odious( long N ) {
+
+    int oneCount = 0;
+    boolean done = false;
+
+    while (!done) {
+      if (N % 2 == 1) {
+        oneCount ++;
+      } 
+
+      N /= 2;
+
+      if (N == 0) {
+        done = true;
+      }
+    }
+
+    if (oneCount % 2 == 1) {
+      return true;
+    }
+    return false;
+
   }
+
   
-  public static void main (String[] args ) 
-  {
+  public static void main (String[] args ) {
     Scanner scan = new Scanner( System.in );
-    long binary; 
-    int N;
+    long N; 
 
     System.out.println( "To exit, enter a negative value" );
     System.out.print( "Enter N: " );
     N = scan.nextInt();
 
-    while ( N >= 0 )
-    {
-      binary = binary( N );
-      System.out.println( "binary is " + binary );
-      System.out.print( "Enter N: " );
+    while ( N >= 0 ) {
+      if (odious(N)) {
+        System.out.println("Odious");
+      }
+      else {
+        System.out.println("Evil");
+      }
+
+      System.out.println( "Enter N: " );
       N = scan.nextInt();
+
+      
     }
      
   }
